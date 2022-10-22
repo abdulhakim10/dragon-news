4b0b7241927ff7280800bf262f6767991104fc3b
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-22 20:40:46 +0600

    Implement Right Side Component and react ions

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaFacebook, FaGithub, FaGoogle, FaTwitter, FaWhatsapp, FaYoutube } from "react-icons/fa";
+import BrandCarousel from '../BrandCaousel/BrandCarousel';
+
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>This is Right side Nav</h2>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
+                <Button className='mb-2' variant='outline-dark'> <FaGithub></FaGithub> Login with Github</Button>   
+            </ButtonGroup>
+            <div>
+                <h5>Find Us</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaYoutube></FaYoutube> YouTube</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel></BrandCarousel>
+            </div>
         </div>
     );
 };

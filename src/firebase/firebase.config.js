d000ad0fb5813c9e8f8fbc518bea6bd4c2260410
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-24 19:09:55 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0oPc4gp2s87s4dpbzlI8GRtYr8HnRxF8",
-  authDomain: "dragon-news-22440.firebaseapp.com",
-  projectId: "dragon-news-22440",
-  storageBucket: "dragon-news-22440.appspot.com",
-  messagingSenderId: "241051970907",
-  appId: "1:241051970907:web:611929a23bb844c37d7e7e"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
